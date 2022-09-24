e9acf049129fcaa868cea39e3f7bd0b439cc8067
Amar Mustafa <amarmustafa2021@gmail.com>
2022-09-24 20:47:21 +0200

    add Counter

--- Counter/app.js
+++ Counter/app.js
@@ -1,3 +1,47 @@
+const decrease = document.getElementById('decrease');
+const reset = document.getElementById('reset');
+const increase = document.getElementById('increase');
+const counter = document.getElementById('number');
+
+let count = 0;
+
+decrease.addEventListener('click', ()=>{
+    count -= 1;
+    counter.innerText = count;
+})
+
+reset.addEventListener('click', ()=>{
+    count = 0;
+    counter.innerText = count;
+})
+
+increase.addEventListener('click', ()=>{
+    count += 1;
+    counter.innerText = count;
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // /////// Selecting each button at a timconst number = document.getElementById('number');
 // const decreaser = document.getElementById('decrease');
 // const reset = document.getElementById('reset');
